0dde37c1e2f8eb1121f90c069c59b8b68b499446
Shannon Foster <shannon.fos3@outlook.com>
2018-04-06 15:59:26 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] skillRatings = {"beginner", "exceptional", "great", "advance", "expert"};
+        String input;
+        int i;
+        Ratings myRating = new Ratings();
+
+        System.out.print("Select a rating between 1 and 5. ");
+        input = sc.nextLine();
+        i = Integer.parseInt(input);
+        myRating.setRate(skillRatings[i]);
+
+            System.out.println("Your skills is " + skillRatings[i]);
+            System.out.print(myRating.getRate());
+            System.out.print('\n');
+            System.out.print(myRating.thisIsMyRating());
+
+    }
+}
